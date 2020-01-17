ac8c023ffb433bcae4a7dc825ae7dd09087725e0
ilkaypolat1985 <ilkaypolat1985@gmail.com>
2020-01-17 15:14:54 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        ProductManager manager = new ProductManager();
+        manager.add(new Product(1, "Book", 49.99));
+    }
+}
---
+++ src/ProductValidator.java
@@ -0,0 +1,9 @@
+public class ProductValidator {
+
+    public static boolean isValid(Product product){
+        if(product.getPrice() > 0 && !product.getName().isEmpty())
+            return true;
+        else
+            return false;
+    }
+}
